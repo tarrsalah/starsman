3a733f715e9131f2c4378bc64385159c0326bdd9
tarrsalah <tarrsalah@gmail.com>
2019-06-11 21:28:53 +0100

    move auth to a standalone plugin.

---
+++ server/auth.js
@@ -0,0 +1,70 @@
+import hapiCookie from "@hapi/cookie";
+import hapiBell from "@hapi/bell";
+
+const auth = {
+  name: "auth",
+  register: async function(server, options) {
+    await server.register(hapiCookie);
+    await server.register(hapiBell);
+    server.auth.strategy("session", "cookie", {
+      cookie: {
+        name: "sid",
+        password: "GQT0QEMI0QKFH0KN48QABSSIHON8H6RP",
+        isSecure: false,
+        path: "/",
+        isSameSite: "Lax"
+      },
+      redirectTo: false
+    });
+
+    server.auth.strategy("github", "bell", {
+      provider: "github",
+      password: "PEBKD2M2FJCFVQU6NJ7D0UDAN78QUTNH",
+      clientId: "d77f5587dd83204b7f1f",
+      clientSecret: "1578f98d970e4c4c49f380679a70b8b3a38c5f04",
+      isSecure: false
+    });
+
+    server.route({
+      method: ["GET"],
+      path: "/auth/github",
+      options: {
+        handler: async (request, h) => {
+          return h.redirect("/auth/github/callback");
+        }
+      }
+    });
+
+    server.route({
+      method: ["GET", "POST"],
+      path: "/auth/github/callback",
+      options: {
+        auth: "github",
+        handler: async (request, h) => {
+          request.cookieAuth.set(request.auth.credentials);
+          return h.redirect("/");
+        }
+      }
+    });
+
+    server.route({
+      method: "GET",
+      path: "/logout",
+      options: {
+        auth: {
+          strategy: "session",
+          mode: "try"
+        },
+        handler: (request, h) => {
+          if (request.auth.isAuthenticated) {
+            request.cookieAuth.clear();
+          }
+
+          return h.redirect("/");
+        }
+      }
+    });
+  }
+};
+
+export default auth;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import path from "path";
 import Hapi from "@hapi/hapi";
 import client from "./client.js";
 import sqlite from "sqlite";
+import auth from "./auth.js";
 
 const db = Promise.resolve()
   .then(() => sqlite.open("./database.sqlite", { Promise }))
@@ -19,69 +20,8 @@ const start = async () => {
       }
     }
   });
-
   await server.register(require("@hapi/inert"));
-  await server.register(require("@hapi/cookie"));
-  await server.register(require("@hapi/bell"));
-
-  server.auth.strategy("session", "cookie", {
-    cookie: {
-      name: "sid",
-      password: "GQT0QEMI0QKFH0KN48QABSSIHON8H6RP",
-      isSecure: false,
-      path: "/",
-      isSameSite: "Lax"
-    },
-    redirectTo: false
-  });
-
-  server.auth.strategy("github", "bell", {
-    provider: "github",
-    password: "PEBKD2M2FJCFVQU6NJ7D0UDAN78QUTNH",
-    clientId: "d77f5587dd83204b7f1f",
-    clientSecret: "1578f98d970e4c4c49f380679a70b8b3a38c5f04",
-    isSecure: false
-  });
-
-  server.route({
-    method: ["GET"],
-    path: "/auth/github",
-    options: {
-      handler: async (request, h) => {
-        return h.redirect("/auth/github/callback");
-      }
-    }
-  });
-
-  server.route({
-    method: ["GET", "POST"],
-    path: "/auth/github/callback",
-    options: {
-      auth: "github",
-      handler: async (request, h) => {
-        request.cookieAuth.set(request.auth.credentials);
-        return h.redirect("/");
-      }
-    }
-  });
-
-  server.route({
-    method: "GET",
-    path: "/logout",
-    options: {
-      auth: {
-        strategy: "session",
-        mode: "try"
-      },
-      handler: (request, h) => {
-        if (request.auth.isAuthenticated) {
-          request.cookieAuth.clear();
-        }
-
-        return h.redirect("/");
-      }
-    }
-  });
+  await server.register(auth);
 
   server.route({
     method: "GET",
