17a8b689b052095f690f6a2a284c4bec7294b6cc
tarrsalah <tarrsalah@gmail.com>
2019-06-18 21:41:08 +0100

    no tag count.

--- client/components/Tag.js
+++ client/components/Tag.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Badge } from "reactstrap";
 
 function Tag({ tag }) {
-  const { text, name, count } = tag;
+  const { text, name } = tag;
 
-  return <span className="tag">{text + "/" + count}</span>;
+  return <span className="tag">{text}</span>;
 }
 
 export default Tag;
