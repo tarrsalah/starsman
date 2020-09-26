548317c367528453527df5749eeff666c3e2c6b0
Salah Eddine Taouririt <tarrsalah@gmail.com>
2020-09-27 00:05:00 +0100

    format.

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
-import {useAuth} from "../auth.js";
+import React, { Component } from "react";
+import { useAuth } from "../auth.js";
 
-import Authenticated from "./Authenticated.js"
-import Unauthenticated from "./Unauthenticated.js"
+import Authenticated from "./Authenticated.js";
+import Unauthenticated from "./Unauthenticated.js";
 
 export default function App() {
-  let {user, isLoading} = useAuth();
+  let { user, isLoading } = useAuth();
   if (isLoading) {
     return null;
   } else {
